02fe183df9e5acb6fc0f7605a9a353216ac217a8
michaelondon <michaelondon6@gmail.com>
2017-08-24 23:27:45 -0400

    Almost done

--- src/BusDriver.java
+++ src/BusDriver.java
@@ -2,44 +2,58 @@ import java.util.Scanner;
 
 public class BusDriver {
 
-	
+	private static final int currentRiders = 0;
+	private static final int scan = 0;
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int nextStep;
-		
-		
-do {
+		String nextStep = "";
+		int currentPassenagers = 0;
+		double totalFare = 0.00;
 
-	
-	
-		System.out.println("Riders On");
-		int ridersGettingOn=input.nextInt();
-
-		System.out.println("Riders off");
-		int ridersGettingOff=input.nextInt();
-		
-	
-		System.out.println(ridersGettingOn +" riders got on at this stop.");
-		
-		System.out.println(ridersGettingOff +" riders got off at this stop.");
-		
-		int currentRiders= ridersGettingOn-ridersGettingOff;
-		
+		do {
 				
-		System.out.println (currentRiders + " currently on the bus.");
-		
-		
-		System.out.println("select 1 to end this route or 2 to proceed to the next stop?");
-		nextStep=input.nextInt();
-		
-		System.out.print("\n");
-		
-	
-		
-}while(nextStep==2);
-   	
+						
+			// Ask driver to input number of passengers getting ON the bus at this stop
+			System.out.println("Passenagers getting On");
+			int passenagersGettingOn = input.nextInt();
 
- 
+			// Ask driver to input number of passengers getting OFF the bus at this stop
+			System.out.println("Passenagers getting off");
+			int passenagersGettingOff = input.nextInt();
+
+			// Show the number of passenger that got ON and OFF at the stop
+			System.out.println(passenagersGettingOn + " passenagers got on at this stop.");
+			System.out.println(passenagersGettingOff + " passenagers got off at this stop.");
+
+			// Math to show the number of passenger currently remaining ON the bus
+			currentPassenagers = currentPassenagers + (passenagersGettingOn - passenagersGettingOff);
+			totalFare = (totalFare + passenagersGettingOn);
+		
+
+			// Show the number of passenger currently remaining ON the bus
+		
+			
+			
+			
+//			 if currentPassenagers=<0{ 
+//				 System.out.println("Total passenager fare $" + totalFare * 1.50);
+//			 
+//			 }
+			System.out.println("Would you like to proceed to the next stop Y/N?");
+			nextStep = input.next().toUpperCase();
+
+			System.out.print("\n");
+
+			// if (currentPassenagers <= 0)
+			
+			
+
+		} while (nextStep.equals("Y"));
+
+		System.out.println(currentPassenagers);
+
+		System.out.println("Total passenager fare $" + totalFare * 1.50);
+
+	}
 }
-}
- 
