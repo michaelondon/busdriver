dda58a9927c70df4d064ecc6e1c2fca0c69466cb
michaelondon <michaelondon6@gmail.com>
2017-08-29 21:16:59 -0400

    Its alive

--- src/BusDriver.java
+++ src/BusDriver.java
@@ -12,8 +12,7 @@ public class BusDriver {
 		double totalFare = 0.00;
 
 		do {
-				
-						
+
 			// Ask driver to input number of passengers getting ON the bus at this stop
 			System.out.println("Passenagers getting On");
 			int passenagersGettingOn = input.nextInt();
@@ -26,34 +25,38 @@ public class BusDriver {
 			System.out.println(passenagersGettingOn + " passenagers got on at this stop.");
 			System.out.println(passenagersGettingOff + " passenagers got off at this stop.");
 
+			System.out.print("\n");
+			
 			// Math to show the number of passenger currently remaining ON the bus
 			currentPassenagers = currentPassenagers + (passenagersGettingOn - passenagersGettingOff);
 			totalFare = (totalFare + passenagersGettingOn);
-		
 
-			// Show the number of passenger currently remaining ON the bus
-		
+			// If there are no more passengers the console will print the total fare.
+			if (currentPassenagers <= 0) {
+				System.out.println("There are no more passenagers on the bus the total fare is $" + totalFare * 1.50);
+			} else if (currentPassenagers >= 0) {
+				System.out.println(currentPassenagers + " passersenger(s) currently on the bus.");
+			}
 			
+			System.out.print("\n");
 			
-			
-//			 if currentPassenagers=<0{ 
-//				 System.out.println("Total passenager fare $" + totalFare * 1.50);
-//			 
-//			 }
+			// if driver types "Y/y" the program will loop back to the beginning.
 			System.out.println("Would you like to proceed to the next stop Y/N?");
 			nextStep = input.next().toUpperCase();
 
 			System.out.print("\n");
 
-			// if (currentPassenagers <= 0)
-			
-			
-
+		
+		
+			// begins the loop.
 		} while (nextStep.equals("Y"));
+		
+	 if (nextStep.equals("N"));
+	System.out.println("Total passenager fare $" + totalFare * 1.50);
 
-		System.out.println(currentPassenagers);
 
-		System.out.println("Total passenager fare $" + totalFare * 1.50);
+		System.out.println("There are " + currentPassenagers + " passersengers remaining on the bus.");
 
-	}
+	
+}
 }
